6533ce92f872bfb4db6bd0136bdbc1306d7d20ac
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-01-03 21:23:10 +0900

    change parameter of dir command.

--- csharp/run-tests.bat
+++ csharp/run-tests.bat
@@ -10,7 +10,7 @@ for /r %%i in (%LOGPREFIX%*.txt) do (
 for /r %%i in (LogLauncher*.exe) do (
 	if exist %%i (
 		echo %%i
-		call :RUN_TEST %%i
+		call :RUN_TEST %%i %~dp0
 	)
 )
 
@@ -20,23 +20,24 @@ GOTO :END
 	@echo on
 	echo     %~dp1
 	set EXE_PATH=%1
+	set PARAM_DIR=%2
 	set EXE_DIR=%~dp1
 
-	%EXE_PATH%                                cmd.exe /c dir c:\
-	%EXE_PATH%                             -- cmd.exe /c dir c:\
+	%EXE_PATH%                                cmd.exe /c dir %PARAM_DIR%
+	%EXE_PATH%                             -- cmd.exe /c dir %PARAM_DIR%
 	%EXE_PATH%                             --
 
-	%EXE_PATH%   -t                        -- cmd.exe /c dir c:\
-	%EXE_PATH%   -tz                       -- cmd.exe /c dir c:\
+	%EXE_PATH%   -t                        -- cmd.exe /c dir %PARAM_DIR%
+	%EXE_PATH%   -tz                       -- cmd.exe /c dir %PARAM_DIR%
 
-	%EXE_PATH%   -t  %EXE_DIR%%LOGPREFIX%-t.txt     -- cmd.exe /c dir c:\
-	%EXE_PATH%   -tz %EXE_DIR%%LOGPREFIX%-tz.txt    -- cmd.exe /c dir c:\
+	%EXE_PATH%   -t  %EXE_DIR%%LOGPREFIX%-t.txt     -- cmd.exe /c dir %PARAM_DIR%
+	%EXE_PATH%   -tz %EXE_DIR%%LOGPREFIX%-tz.txt    -- cmd.exe /c dir %PARAM_DIR%
 
-	%EXE_PATH% -a -t  %EXE_DIR%%LOGPREFIX%-t-a.txt  -- cmd.exe /c dir c:\
-	%EXE_PATH% -a -t  %EXE_DIR%%LOGPREFIX%-t-a.txt  -- cmd.exe /c dir c:\
+	%EXE_PATH% -a -t  %EXE_DIR%%LOGPREFIX%-t-a.txt  -- cmd.exe /c dir %PARAM_DIR%
+	%EXE_PATH% -a -t  %EXE_DIR%%LOGPREFIX%-t-a.txt  -- cmd.exe /c dir %PARAM_DIR%
 
-	%EXE_PATH% -a -tz %EXE_DIR%%LOGPREFIX%-tz-a.txt -- cmd.exe /c dir c:\
-	%EXE_PATH% -a -tz %EXE_DIR%%LOGPREFIX%-tz-a.txt -- cmd.exe /c dir c:\
+	%EXE_PATH% -a -tz %EXE_DIR%%LOGPREFIX%-tz-a.txt -- cmd.exe /c dir %PARAM_DIR%
+	%EXE_PATH% -a -tz %EXE_DIR%%LOGPREFIX%-tz-a.txt -- cmd.exe /c dir %PARAM_DIR%
 	@echo off
 
 goto :EOF
