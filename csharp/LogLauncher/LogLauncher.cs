894dda752d4c39e632eb3deac90f52058c350153
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-01-03 12:11:54 +0900

    move core function to separate source.

---
+++ csharp/LogLauncher/LogLauncher.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace LogLauncher
+{
+    public class LogLauncher
+    {
+        public static int Launch(string[] args)
+        {
+            string argument = string.Empty;
+            if (args.Length < 1)
+            {
+                return 1;
+            }
+            else if (args.Length > 1)
+            {
+                var new_length = args.Length - 1;
+                string[] arguments = new string[new_length];
+                Array.Copy(args, 1, arguments, 0, new_length);
+                argument = string.Join(" ", arguments);
+            }
+
+            var process = new Process();
+            process.StartInfo.FileName = args[0];
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.StartInfo.RedirectStandardInput = false;
+
+            process.StartInfo.CreateNoWindow = false;
+            process.StartInfo.Arguments = argument;
+            process.OutputDataReceived += new DataReceivedEventHandler(delegate (object obj, DataReceivedEventArgs e)
+            {
+                Console.WriteLine(e.Data);
+            });
+            process.ErrorDataReceived += new DataReceivedEventHandler(delegate (object obj, DataReceivedEventArgs e)
+            {
+                Console.WriteLine(e.Data);
+            });
+
+            try
+            {
+                process.Start();
+           
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+                process.WaitForExit();
+
+                return process.ExitCode;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return 1;
+            }
+        }
+    }
+}
