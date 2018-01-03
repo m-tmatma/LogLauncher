5eaa5bf427839e24d95c841ab7d7dea0d063ee70
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-01-03 21:06:03 +0900

    add indent

--- csharp/run-tests.bat
+++ csharp/run-tests.bat
@@ -17,27 +17,27 @@ for /r %%i in (LogLauncher*.exe) do (
 GOTO :END
 
 :RUN_TEST
-@echo on
-echo     %~dp1
-set EXE_PATH=%1
-set EXE_DIR=%~dp1
+	@echo on
+	echo     %~dp1
+	set EXE_PATH=%1
+	set EXE_DIR=%~dp1
 
-%EXE_PATH%                                cmd.exe /c dir c:\
-%EXE_PATH%                             -- cmd.exe /c dir c:\
-%EXE_PATH%                             --
+	%EXE_PATH%                                cmd.exe /c dir c:\
+	%EXE_PATH%                             -- cmd.exe /c dir c:\
+	%EXE_PATH%                             --
 
-%EXE_PATH%   -t                        -- cmd.exe /c dir c:\
-%EXE_PATH%   -tz                       -- cmd.exe /c dir c:\
+	%EXE_PATH%   -t                        -- cmd.exe /c dir c:\
+	%EXE_PATH%   -tz                       -- cmd.exe /c dir c:\
 
-%EXE_PATH%   -t  %EXE_DIR%%LOGPREFIX%-t.txt     -- cmd.exe /c dir c:\
-%EXE_PATH%   -tz %EXE_DIR%%LOGPREFIX%-tz.txt    -- cmd.exe /c dir c:\
+	%EXE_PATH%   -t  %EXE_DIR%%LOGPREFIX%-t.txt     -- cmd.exe /c dir c:\
+	%EXE_PATH%   -tz %EXE_DIR%%LOGPREFIX%-tz.txt    -- cmd.exe /c dir c:\
 
-%EXE_PATH% -a -t  %EXE_DIR%%LOGPREFIX%-t-a.txt  -- cmd.exe /c dir c:\
-%EXE_PATH% -a -t  %EXE_DIR%%LOGPREFIX%-t-a.txt  -- cmd.exe /c dir c:\
+	%EXE_PATH% -a -t  %EXE_DIR%%LOGPREFIX%-t-a.txt  -- cmd.exe /c dir c:\
+	%EXE_PATH% -a -t  %EXE_DIR%%LOGPREFIX%-t-a.txt  -- cmd.exe /c dir c:\
 
-%EXE_PATH% -a -tz %EXE_DIR%%LOGPREFIX%-tz-a.txt -- cmd.exe /c dir c:\
-%EXE_PATH% -a -tz %EXE_DIR%%LOGPREFIX%-tz-a.txt -- cmd.exe /c dir c:\
-@echo off
+	%EXE_PATH% -a -tz %EXE_DIR%%LOGPREFIX%-tz-a.txt -- cmd.exe /c dir c:\
+	%EXE_PATH% -a -tz %EXE_DIR%%LOGPREFIX%-tz-a.txt -- cmd.exe /c dir c:\
+	@echo off
 
 goto :EOF
 
